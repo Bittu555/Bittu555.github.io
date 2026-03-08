4b510562b702eec431603c2efea933fbc04b02c3
Bittu Kumar <84106956+Bittu555@users.noreply.github.com>
2026-03-08 15:43:58 +0530

    Refactor index.js to configure typed.js
    Removed hamburger menu functionality and added typed.js configuration.

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-//index.js
+var typed = new Typed(".typing",{
 
-const hamburger = document.getElementById('hamburger'); 
-const menu = document.querySelector('.menu'); 
+strings:[
+"Senior Java Backend Engineer",
+"Microservices Developer",
+"Cloud & Distributed Systems Engineer"
+],
 
-hamburger.addEventListener('click', function () { 
-    const hamIcon = this.querySelector('.hamburger-icon'); 
-    const crossIcon = this.querySelector('.cross-icon'); 
-    if (hamIcon.style.display === "none") { 
-        hamIcon.style.display = "inline-block"
-        menu.style.display = "none"
-        crossIcon.style.display = "none"
-    } 
-    else { 
-        crossIcon.style.display = "inline-block"
-        hamIcon.style.display = "none"
-        menu.style.display = "block"
-    } 
+typeSpeed:60,
+backSpeed:40,
+loop:true
+
 });
